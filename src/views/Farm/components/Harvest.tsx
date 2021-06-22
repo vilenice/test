f794dd7d5dd2e0305e9f3ce63dd94b2a97c3817a
Mars2009 <83943855+Mars2009@users.noreply.github.com>
2021-06-22 22:44:10 +0800

    o

--- src/views/Farm/components/Harvest.tsx
+++ src/views/Farm/components/Harvest.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
-
+import Button from '../../../components/Button';
 import Card from '../../../components/Card';
 import CardContent from '../../../components/CardContent';
 import CardIcon from '../../../components/CardIcon';
 import Label from '../../../components/Label';
 import Value from '../../../components/Value';
+import useEarnings from '../../../hooks/useEarnings';
+import { getDisplayBalance } from '../../../utils/formatBalance';
+import useHarvest from '../../../hooks/useHarvest';
 
 import TokenSymbol from '../../../components/TokenSymbol';
 import { Farm } from '../../../go-farm';
@@ -15,6 +18,8 @@ interface HarvestProps {
 }
 
 const Harvest: React.FC<HarvestProps> = ({ farm }) => {
+  const earnings = useEarnings(farm.pid);
+  const { onReward } = useHarvest(farm);
   return (
     <Card>
       <CardContent>
@@ -23,18 +28,24 @@ const Harvest: React.FC<HarvestProps> = ({ farm }) => {
             <CardIcon>
               <TokenSymbol symbol={farm.earnToken.symbol} />
             </CardIcon>
-            <Value value={'0'} />
+            <Value value={getDisplayBalance(earnings)} />
+
             <Label text={`赚到的${farm.earnTokenName}`} />
           </StyledCardHeader>
-          {/* <StyledCardActions>
+          <StyledCardActions>
             <Button onClick={onReward} disabled={earnings.eq(0)} text="收获"  />
-          </StyledCardActions> */}
+          </StyledCardActions>
         </StyledCardContentInner>
       </CardContent>
     </Card>
   );
 };
-
+const StyledCardActions = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: ${(props) => props.theme.spacing[6]}px;
+  width: 100%;
+`;
 const StyledCardHeader = styled.div`
   align-items: center;
   display: flex;
